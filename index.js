27b4f0c9899ba0eb0efc77d38b7ebf7449c2302a
Victor Scandolara <victor.scandolara@gmail.com>
2017-08-12 20:06:39 -0300

    criação do método de teste de parametro

--- index.js
+++ index.js
@@ -98,8 +98,8 @@ app.post('/usuario',function(request, response){
 });
 
 app.post('/teste-param',function(request, response){
-	var tok = request.body.token;
-	response.send(JSON.stringify({token:tok},null,3));
+	//var tok = request.body.token;
+	response.send(JSON.stringify(request.body,null,3));
 });
 
 app.listen(app.get('port'), function() {
